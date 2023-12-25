967ca8b2636f12c6067cef62e153e88e194a9a20
aghArdeshir <ardeshirizadinl@gmail.com>
2023-12-25 18:30:52 +0100

    Music: bring in all music stuff to this repo

---
+++ music/index.mjs
@@ -0,0 +1,109 @@
+import { songs } from './songs/songs.mjs';
+
+const song = [
+  songs.aghArdeshir.badAzBad,
+  songs.nishkhand.sarbasteyeYekSarbaz,
+  songs.aghArdeshir.tann4,
+  songs.aghArdeshir.mordBaram,
+][3];
+
+fetch(song.lyricsUrl)
+  .then((res) => res.json())
+  .then((lyrics) => {
+    document.getElementsByClassName('cover-art')[0].src = song.coverUrl;
+    document.querySelector('.title-persian').textContent = song.title;
+    document.querySelector('.artist-persian').textContent = song.artist;
+    document.body.style.color = song.textColor;
+
+    const backgroundContainerDom = document.querySelector(
+      '.background-container'
+    );
+    backgroundContainerDom.style.backgroundImage = `url("${song.coverUrl}")`;
+
+    const secondaryBackgroundDom = document.querySelector(
+      '.secondary-background'
+    );
+    secondaryBackgroundDom.style.backgroundColor = song.bgColor;
+
+    const animationDuration = 0.3;
+    const cssTransition = `all ${animationDuration}s ease-out`;
+    const cssTop = '10px';
+    const cssFontSize = '18px';
+    const cssSmallFontSize = '0px';
+
+    const lyricsDom = document.getElementById('lyrics');
+    const lyricsBackupDom = document.getElementById('lyrics-backup');
+
+    let lyricsText = '';
+    let lyricsBackupText = '';
+
+    function setLyricsText(text = '') {
+      if (lyricsText !== text) {
+        lyricsText = text || '';
+
+        lyricsDom.style.transition = '';
+        lyricsDom.style.top = '90px';
+        lyricsDom.style.fontSize = cssSmallFontSize;
+        lyricsDom.style.opacity = '0.5';
+
+        lyricsDom.innerHTML = lyricsText;
+
+        setTimeout(() => {
+          lyricsDom.style.transition = cssTransition;
+          lyricsDom.style.top = cssTop;
+          lyricsDom.style.fontSize = cssFontSize;
+          lyricsDom.style.opacity = '1';
+        }, animationDuration * 1000);
+      }
+    }
+
+    function setLyricsBackupText(text = '') {
+      if (lyricsBackupText !== text) {
+        lyricsBackupText = text || '';
+
+        lyricsBackupDom.style.transition = '';
+        lyricsBackupDom.style.top = '50px';
+        lyricsBackupDom.style.fontSize = cssFontSize;
+        lyricsBackupDom.style.opacity = '1';
+
+        lyricsBackupDom.innerHTML = lyricsBackupText;
+
+        setTimeout(() => {
+          lyricsBackupDom.style.transition = cssTransition;
+          lyricsBackupDom.style.top = cssTop;
+          lyricsBackupDom.style.fontSize = cssSmallFontSize;
+          lyricsBackupDom.style.opacity = '0.5';
+        }, animationDuration * 1000);
+      }
+    }
+
+    const wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+    });
+    wavesurfer.load(song.songUrl);
+    wavesurfer.on('audioprocess', function (currentTime) {
+      const index =
+        lyrics.findIndex(
+          (member) => member.time - animationDuration >= currentTime
+        ) - 1;
+
+      if (index < 0) {
+        lyricsDom.innerHTML = '';
+        return;
+      }
+
+      if (index === 0 && lyrics[index].time - animationDuration > currentTime) {
+        lyricsDom.innerHTML = '';
+        return;
+      }
+
+      setLyricsText(lyrics[index].text);
+      if (lyrics[index - 1]) {
+        setLyricsBackupText(lyrics[index - 1].text);
+      }
+    });
+
+    setTimeout(() => {
+      wavesurfer.play();
+    }, 5000);
+  });
---
+++ music/songs/songs.mjs
@@ -0,0 +1,46 @@
+export const songs = {
+  aghArdeshir: {
+    badAzBad: {
+      coverUrl:
+        "songs/aghArdeshir - Ba'd Az Bad/ba'd-az-bad.original.1800x1800.png",
+      songUrl: "songs/aghArdeshir - Ba'd Az Bad/aghArdeshir - Ba'd Az Bad.mp3",
+      lyricsUrl: "songs/aghArdeshir - Ba'd Az Bad/ba'd-az-bad.lyrics.json",
+      bgColor: '#252527',
+      textColor: 'white',
+      title: 'بعد از بد',
+      artist: 'آق‌اردشیر',
+    },
+    tann4: {
+      coverUrl: 'songs/aghArdeshir - 4.5/4-30-cover.png',
+      songUrl: 'songs/aghArdeshir - 4.5/aghArdeshir - TANN 4 (4.5).mp3',
+      lyricsUrl: 'songs/aghArdeshir - 4.5/tann4.lyrics.json',
+      bgColor: 'black',
+      textColor: 'white',
+      title: 'چار و نیم',
+      artist: 'آق‌اردشیر',
+    },
+    mordBaram: {
+      coverUrl: 'songs/aghArdeshir - Mord Baram/Mord Baram Cover.png',
+      songUrl: 'songs/aghArdeshir - Mord Baram/aghArdeshir - Mord Baram.mp3',
+      lyricsUrl: 'songs/aghArdeshir - Mord Baram/MordBaram.lyrics.json',
+      bgColor: 'rgb(0, 4, 27)',
+      textColor: 'white',
+      title: 'مُرد برام',
+      artist: 'آق‌اردشیر',
+    },
+  },
+  nishkhand: {
+    sarbasteyeYekSarbaz: {
+      coverUrl:
+        'songs/Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.cover.1200x1200.png',
+      songUrl:
+        'songs/Nishkhand - Sarbasteye Yek Sarbaz/Nishkhand - Sarbasteye Yek Sarbaz 128.mp3',
+      lyricsUrl:
+        'songs/Nishkhand - Sarbasteye Yek Sarbaz/sarbasteye-yek-sarbaz.lyrics.json',
+      bgColor: 'white',
+      textColor: 'black',
+      title: 'سربسته‌ی یک سرباز',
+      artist: 'نیشخند',
+    },
+  },
+};
