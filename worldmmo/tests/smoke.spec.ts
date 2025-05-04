aee9a745d0d97a66e509e94a4017fd36d0e26a71
aghArdeshir <ardeshirizadinl@gmail.com>
2025-05-04 16:34:28 +0200

    worldmmo test: write test to make sure all cells are rendererd

--- worldmmo/tests/smoke.spec.ts
+++ worldmmo/tests/smoke.spec.ts
@@ -4,3 +4,10 @@ test('initial smoke test', async ({ page }) => {
 	await page.goto('/');
 	await expect(page).toHaveTitle(/World MMO/);
 });
+
+test('renders all cels', async ({ page }) => {
+  await page.goto('/');
+  const cells = await page.locator('.cell');
+  const celCount = await cells.count();
+  expect(celCount).toBe(800);
+});
