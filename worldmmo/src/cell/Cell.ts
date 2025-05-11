5cff5e0b46b0338dfd5f4e1af2508cb5b0fde89e
aghArdeshir <ardeshirizadinl@gmail.com>
2025-05-11 12:06:23 +0200

    remove unused ignore trule

--- worldmmo/src/cell/Cell.ts
+++ worldmmo/src/cell/Cell.ts
@@ -1,6 +1,5 @@
 import { CellPopup } from "./CellPopup";
 
-// biome-ignore lint/complexity/noStaticOnlyClass: Will be done later
 export class Cell {
   static CELL_WIDTH = 40;
   static CELL_HEIGHT = 40;
