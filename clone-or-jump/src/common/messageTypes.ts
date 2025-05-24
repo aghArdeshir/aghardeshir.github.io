e74717e1aa64a559356880ee1dfb873dde710c7e
aghArdeshir <ardeshirizadinl@gmail.com>
2025-05-24 10:55:02 +0200

    improve MessageTypes structure

--- clone-or-jump/src/common/messageTypes.ts
+++ clone-or-jump/src/common/messageTypes.ts
@@ -1,18 +1,25 @@
+type anyMessage = Record<string, unknown>;
+
+// NEW PLAYER JOINED
+
+const newPlayerJoined = "newPlayerJoined";
+
 type MessageNewPlayerJoined = {
-  messageId: "newPlayerJoined";
+  messageId: typeof newPlayerJoined;
 };
 
 export function generateMessageNewPlayerJoined(): MessageNewPlayerJoined {
   return {
-    messageId: "newPlayerJoined",
+    messageId: newPlayerJoined,
   };
 }
 
 export function isMessageNewPlayerJoined(
-  message: Record<string, unknown>
+  message: anyMessage
 ): message is MessageNewPlayerJoined {
   return (
     "messageId" in message &&
+    (message as MessageNewPlayerJoined).messageId === newPlayerJoined
     (message as MessageNewPlayerJoined).messageId === "newPlayerJoined"
   );
 }
