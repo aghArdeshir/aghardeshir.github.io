de8097de7d3ab89119671510816afce27378d2b4
aghArdeshir <ardeshirizadinl@gmail.com>
2025-06-01 20:02:06 +0200

    clone-or-jump: player 2 clones to left ready to attack

--- clone-or-jump/tests/game.spec.ts
+++ clone-or-jump/tests/game.spec.ts
@@ -505,6 +505,191 @@ test("Play Game: 1v1 4x4", async ({ page: originalPage_DontUse, browser }) => {
       await expect(cell).toBeVisible();
     }
   });
+
+  await test.step("player 1 jumps to the right", async () => {
+    const cellToJump = player_1_page.locator(
+      `[data-testid="my-cell"][data-x="1"][data-y="0"]`
+    );
+    await cellToJump.click();
+
+    const availableClones = [
+      player_1_page.locator(`[available-clone][data-x="2"][data-y="0"]`),
+      player_1_page.locator(`[available-clone][data-x="1"][data-y="1"]`),
+    ];
+    const availableJumps = [
+      player_1_page.locator(`[available-jump][data-x="3"][data-y="0"]`),
+      player_1_page.locator(`[available-jump][data-x="1"][data-y="2"]`),
+    ];
+    await test.step("assert move hints are visible", async () => {
+      for (const cell of [...availableClones, ...availableJumps]) {
+        await expect(cell).toBeVisible();
+      }
+    });
+    await availableJumps[0].click(); // jump to the right
+    await test.step("assert move hints are no longer visible", async () => {
+      for (const cell of [...availableClones, ...availableJumps]) {
+        await expect(cell).not.toBeVisible();
+      }
+    });
+  });
+
+  await test.step("player 2 clones to the top", async () => {
+    const cellToClone = player_2_page.locator(
+      `[data-testid="my-cell"][data-x="3"][data-y="2"]`
+    );
+    await cellToClone.click();
+
+    const availableClones = [
+      player_2_page.locator(`[available-clone][data-x="3"][data-y="1"]`),
+      player_2_page.locator(`[available-clone][data-x="2"][data-y="2"]`),
+    ];
+    const availableJumps = [
+      player_2_page.locator(`[available-jump][data-x="1"][data-y="2"]`),
+    ];
+
+    await test.step("assert move hints are visible", async () => {
+      for (const cell of [...availableClones, ...availableJumps]) {
+        await expect(cell).toBeVisible();
+      }
+    });
+
+    await test.step("jump to the enemy cell should not be available", async () => {
+      await expect(
+        player_2_page.locator(`[available-jump][data-x="3"][data-y="0"]`)
+      ).not.toBeVisible();
+    });
+
+    await availableClones[0].click(); // clone to the top
+
+    await test.step("assert move hints are no longer visible", async () => {
+      for (const cell of [...availableClones, ...availableJumps]) {
+        await expect(cell).not.toBeVisible();
+      }
+    });
+  });
+
+  await test.step("player 1 clones to right", async () => {
+    const player1CellToClone = player_1_page.locator(
+      `[data-testid="my-cell"][data-x="0"][data-y="0"]`
+    );
+    await player1CellToClone.click();
+    const player1RightClone = player_1_page.locator(
+      `[available-clone][data-x="1"][data-y="0"]`
+    );
+    await player1RightClone.click();
+  });
+
+  await test.step("player 2 clones left", async () => {
+    const player2CellToClone = player_2_page.locator(
+      `[data-testid="my-cell"][data-x="3"][data-y="1"]`
+    );
+    await player2CellToClone.click();
+    const player2LeftClone = player_2_page.locator(
+      `[available-clone][data-x="2"][data-y="1"]`
+    );
+    await player2LeftClone.click();
+  });
+
+  await test.step("player 1 clones bottom", async () => {
+    const player1CellToClone = player_1_page.locator(
+      `[data-testid="my-cell"][data-x="0"][data-y="0"]`
+    );
+    await player1CellToClone.click();
+    const player1BottomClone = player_1_page.locator(
+      `[available-clone][data-x="0"][data-y="1"]`
+    );
+    await player1BottomClone.click();
+  });
+
+  await test.step("both players should see game state correctly", async () => {
+    await test.step("player 1 sees own and enemy cells", async () => {
+      const player1Cells = [
+        player_1_page.locator(
+          `[data-testid="my-cell"][data-x="0"][data-y="0"]`
+        ),
+        player_1_page.locator(
+          `[data-testid="my-cell"][data-x="1"][data-y="0"]`
+        ),
+        player_1_page.locator(
+          `[data-testid="my-cell"][data-x="0"][data-y="1"]`
+        ),
+      ];
+      const player2Cells = [
+        player_1_page.locator(
+          `[data-testid="enemy-cell"][data-x="3"][data-y="3"]`
+        ),
+        player_1_page.locator(
+          `[data-testid="enemy-cell"][data-x="3"][data-y="2"]`
+        ),
+        player_1_page.locator(
+          `[data-testid="enemy-cell"][data-x="3"][data-y="1"]`
+        ),
+        player_1_page.locator(
+          `[data-testid="enemy-cell"][data-x="3"][data-y="0"]`
+        ),
+        player_1_page.locator(
+          `[data-testid="enemy-cell"][data-x="2"][data-y="1"]`
+        ),
+      ];
+
+      for (const cell of [...player1Cells, ...player2Cells]) {
+        await expect(cell).toBeVisible();
+      }
+    });
+
+    await test.step("player 2 sees own and enemy cells", async () => {
+      const player1Cells = [
+        player_2_page.locator(
+          `[data-testid="enemy-cell"][data-x="0"][data-y="0"]`
+        ),
+        player_2_page.locator(
+          `[data-testid="enemy-cell"][data-x="1"][data-y="0"]`
+        ),
+        player_2_page.locator(
+          `[data-testid="enemy-cell"][data-x="0"][data-y="1"]`
+        ),
+      ];
+      const player2Cells = [
+        player_2_page.locator(
+          `[data-testid="my-cell"][data-x="3"][data-y="3"]`
+        ),
+        player_2_page.locator(
+          `[data-testid="my-cell"][data-x="3"][data-y="2"]`
+        ),
+        player_2_page.locator(
+          `[data-testid="my-cell"][data-x="3"][data-y="1"]`
+        ),
+        player_2_page.locator(
+          `[data-testid="my-cell"][data-x="3"][data-y="0"]`
+        ),
+        player_2_page.locator(
+          `[data-testid="my-cell"][data-x="2"][data-y="1"]`
+        ),
+      ];
+
+      for (const cell of [...player1Cells, ...player2Cells]) {
+        await expect(cell).toBeVisible();
+      }
+    });
+
+    const emptyCells = [
+      `[data-testid="empty-cell"][data-x="2"][data-y="0"]`,
+      `[data-testid="empty-cell"][data-x="1"][data-y="1"]`,
+      `[data-testid="empty-cell"][data-x="0"][data-y="2"]`,
+      `[data-testid="empty-cell"][data-x="1"][data-y="2"]`,
+      `[data-testid="empty-cell"][data-x="2"][data-y="2"]`,
+      `[data-testid="empty-cell"][data-x="0"][data-y="3"]`,
+      `[data-testid="empty-cell"][data-x="1"][data-y="3"]`,
+      `[data-testid="empty-cell"][data-x="2"][data-y="3"]`,
+    ];
+
+    expect(emptyCells.length).toBe(8);
+
+    for (const cell of emptyCells) {
+      await expect(player_1_page.locator(cell)).toBeVisible();
+      await expect(player_2_page.locator(cell)).toBeVisible();
+    }
+  });
 });
 
 test.afterEach(async () => {
