55b3527657fa0c647050486aaa402a3f87725579
aghArdeshir <ardeshirizadinl@gmail.com>
2025-11-02 17:40:30 +0100

    clone-or-jump: shell.nix to run playwright tests in nixos

---
+++ clone-or-jump/shell.nix
@@ -0,0 +1,17 @@
+# To run playwright tests:
+# ```
+#   nix-shell
+#   npm test
+# ```
+
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      playwright-driver.browsers
+    ];
+
+    shellHook = ''
+      export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+      export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+    '';
+}
