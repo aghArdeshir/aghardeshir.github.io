b256015055ef06550c016162c36db641eb061aca
aghArdeshir <ardeshirizadinl@gmail.com>
2025-11-19 22:44:23 +0100

    refactor: blob -> blobUrl

--- Track.ts
+++ Track.ts
@@ -1,18 +1,20 @@
 export class Track {
   private file: File;
-  private blob: string;
+  private blobUrl: string;
   public name: string;
 
   constructor() {}
 
   setFile(file: File) {
     this.file = file;
-    this.blob = URL.createObjectURL(file);
     this.setName(file.name);
+
+    // TODO: revoke the blob URL when the track is deleted
+    this.blobUrl = URL.createObjectURL(file);
   }
 
   play() {
-    const audio = new Audio(this.blob);
+    const audio = new Audio(this.blobUrl);
     audio.play();
   }
 
