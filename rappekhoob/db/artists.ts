64ca1488789f76f4c25bf0d5259d58686c3165e9
aghArdeshir <ardeshirizadinl@gmail.com>
2025-12-25 00:02:28 +0100

    rappekhoob: add new artists

--- rappekhoob/db/artists.ts
+++ rappekhoob/db/artists.ts
@@ -32,6 +32,154 @@ const Veda: Artist = {
   name: "Veda",
 };
 
+const Mezrab: Artist = {
+  name: "Mezrab",
+};
+const Farsea: Artist = {
+  name: "Farsea",
+};
+const Rez: Artist = {
+  name: "Rez",
+};
+const Canis: Artist = {
+  name: "Canis",
+};
+const Navid: Artist = {
+  name: "Navid",
+};
+const Yara: Artist = {
+  name: "Yara",
+};
+const Farshad: Artist = {
+  name: "Farshad",
+};
+const PooYar: Artist = {
+  name: "PooYar",
+};
+const Najwa: Artist = {
+  name: "Najwa",
+};
+const Dehghan: Artist = {
+  name: "Dehghan",
+};
+const Shemrooni: Artist = {
+  name: "Shemrooni",
+};
+const ShahabAsgari: Artist = {
+  name: "ShahabAsgari",
+};
+const Naraka: Artist = {
+  name: "Naraka",
+};
+const Ho3ein: Artist = {
+  name: "Ho3ein",
+};
+const Shayea: Artist = {
+  name: "Shayea",
+};
+const TDey: Artist = {
+  name: "TDey",
+};
+const Quf: Artist = {
+  name: "Quf",
+};
+const Bidad: Artist = {
+  name: "Bidad",
+};
+const _7Khat: Artist = {
+  name: "7Khat",
+};
+const Lowkey: Artist = {
+  name: "Lowkey",
+};
+const Fadaei: Artist = {
+  name: "Fadaei",
+};
+const Poornam: Artist = {
+  name: "Poornam",
+};
+const Andish: Artist = {
+  name: "Andish",
+};
+const Dahesh: Artist = {
+  name: "Dahesh",
+};
+const Sayf: Artist = {
+  name: "Sayf",
+};
+const Kenzler: Artist = {
+  name: "Kenzler",
+};
+const Sam: Artist = {
+  name: "Sam",
+};
+const MoeinFallah: Artist = {
+  name: "Moein Fallah",
+};
+const Parish: Artist = {
+  name: "Parish",
+};
+const Faghan: Artist = {
+  name: "Faghan",
+};
+const SepehrBeigi: Artist = {
+  name: "Sepehr Beigi",
+};
+const Ebrahim: Artist = {
+  name: "Ebrahim",
+};
+const MimKaaf: Artist = {
+  name: "MimKaaf",
+};
+const AshiBeatz: Artist = {
+  name: "AshiBeatz",
+};
+const Reveal: Artist = {
+  name: "Reveal",
+};
+const Hidden: Artist = {
+  name: "Hidden",
+};
+const Mahyar: Artist = {
+  name: "Mahyar",
+};
+const TaraSalahi: Artist = {
+  name: "TaraSalahi",
+};
+const Khalvat: Artist = {
+  name: "Khalvat",
+};
+const SinaSae: Artist = {
+  name: "SinaSae",
+};
+const Deon: Artist = {
+  name: "Deon",
+};
+const Catchybeatz: Artist = {
+  name: "Catchybeatz",
+};
+const Zaal: Artist = {
+  name: "Zaal",
+};
+const Hoomaan: Artist = {
+  name: "Hoomaan",
+};
+const Maslak: Artist = {
+  name: "Maslak",
+};
+const Poori: Artist = {
+  name: "Poori",
+};
+const Daniyal: Artist = {
+  name: "Daniyal",
+};
+const Taniya: Artist = {
+  name: "Taniya",
+};
+const Pishro: Artist = {
+  name: "Pishro",
+};
+
 export const artists = {
   Hichkas,
   Sadegh,
@@ -41,4 +189,53 @@ export const artists = {
   Atour,
   Sorena,
   Veda,
+  Mezrab,
+  Farsea,
+  Rez,
+  Canis,
+  Navid,
+  Yara,
+  Farshad,
+  PooYar,
+  Najwa,
+  Dehghan,
+  Shemrooni,
+  ShahabAsgari,
+  Naraka,
+  Ho3ein,
+  Shayea,
+  TDey,
+  Quf,
+  Bidad,
+  "7Khat": _7Khat,
+  Lowkey,
+  Fadaei,
+  Poornam,
+  Andish,
+  Dahesh,
+  Sayf,
+  Kenzler,
+  Sam,
+  MoeinFallah,
+  Parish,
+  Faghan,
+  SepehrBeigi,
+  Ebrahim,
+  MimKaaf,
+  AshiBeatz,
+  Reveal,
+  Hidden,
+  Mahyar,
+  TaraSalahi,
+  Khalvat,
+  SinaSae,
+  Deon,
+  Catchybeatz,
+  Zaal,
+  Hoomaan,
+  Maslak,
+  Poori,
+  Daniyal,
+  Taniya,
+  Pishro,
 };
