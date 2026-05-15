11c4318b4dab27d31b37b3c7ceeacc7b3abff94d
aghArdeshir <ardeshirizadinl@gmail.com>
2026-05-16 01:55:21 +0200

    make ball collision detection a bit smoother

--- recreational/ball-rectangle/Ball.js
+++ recreational/ball-rectangle/Ball.js
@@ -2,6 +2,7 @@ export class Ball {
   radius = 20;
   direction = { x: Math.random(), y: Math.random() };
   speed = 0.4;
+  color = "green";
 
   constructor({ centerPoint }) {
     this.x = centerPoint.x;
@@ -9,7 +10,7 @@ export class Ball {
   }
 
   draw(ctx) {
-    ctx.fillStyle = "green";
+    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
@@ -20,10 +21,10 @@ export class Ball {
     this.x += (this.direction.x / magnitude) * this.speed * deltaTime;
     this.y += (this.direction.y / magnitude) * this.speed * deltaTime;
 
-    const hitRightWall = this.x + this.radius > rectangle.topRight.x;
-    const hitLeftWall = this.x - this.radius < rectangle.topLeft.x;
-    const hitBottomWall = this.y + this.radius > rectangle.bottomLeft.y;
-    const hitTopWall = this.y - this.radius < rectangle.topLeft.y;
+    const hitRightWall = this.x + this.radius >= rectangle.topRight.x;
+    const hitLeftWall = this.x - this.radius <= rectangle.topLeft.x;
+    const hitBottomWall = this.y + this.radius >= rectangle.bottomLeft.y;
+    const hitTopWall = this.y - this.radius <= rectangle.topLeft.y;
 
     if (hitRightWall) {
       this.direction.x = -1;
@@ -35,6 +36,18 @@ export class Ball {
       this.direction.y = -1;
     } else if (hitTopWall) {
       this.direction.y = 1;
+    }
+
+    if (hitRightWall) {
+      this.x = Math.min(rectangle.topRight.x - this.radius - 1, this.x);
+    } else if (hitLeftWall) {
+      this.x = Math.max(rectangle.topLeft.x + this.radius + 1, this.x);
+    }
+
+    if (hitBottomWall) {
+      this.y = Math.min(rectangle.bottomLeft.y - this.radius - 1, this.y);
+    } else if (hitTopWall) {
+      this.y = Math.max(rectangle.topLeft.y + this.radius + 1, this.y);
     }
   }
 }
